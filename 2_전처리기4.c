f4474a35aab152d3a7231e873c7545763ed3658f
Chansik Yun <chansigi@gmail.com>
2023-07-24 13:14:04 +0900

    .

--- 2_전처리기4.c
+++ 2_전처리기4.c
@@ -1,6 +1,7 @@
 // 2_전처리기4.c
 #include <stdio.h>
 
+#if 0
 int cnt = 0;
 
 void print_cnt(void)
@@ -9,12 +10,39 @@ void print_cnt(void)
 }
 
 #define CALL_FUNC(fp) (++cnt, fp())
+// 위의 함수를 일반 함수(인라인 함수)로 변경해야 합니다.
 
 int main(void)
 {
-    CALL_FUNC(print_cnt);
-    CALL_FUNC(print_cnt);
-    CALL_FUNC(print_cnt);
+    // cnt라는 지역 변수가 존재하면, 아래 코드는 결과가 제대로 나오지 않습니다.
+    int cnt = 10;
+
+    CALL_FUNC(print_cnt); // (++cnt, print_cnt());
+    CALL_FUNC(print_cnt); // (++cnt, print_cnt());
+    CALL_FUNC(print_cnt); // (++cnt, print_cnt());
+
+    return 0;
+}
+#endif
+
+int cnt = 0;
+
+void print_cnt(void)
+{
+    printf("cnt = %d\n", cnt);
+}
+
+#define CALL_FUNC(fp) (++cnt, fp())
+// 위의 함수를 일반 함수(인라인 함수)로 변경해야 합니다.
+
+int main(void)
+{
+    // cnt라는 지역 변수가 존재하면, 아래 코드는 결과가 제대로 나오지 않습니다.
+    int cnt = 10;
+
+    CALL_FUNC(print_cnt); // (++cnt, print_cnt());
+    CALL_FUNC(print_cnt); // (++cnt, print_cnt());
+    CALL_FUNC(print_cnt); // (++cnt, print_cnt());
 
     return 0;
 }
