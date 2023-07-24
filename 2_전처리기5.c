70e0060be0f45ff1eb18371bc7761ea109afc5da
Chansik Yun <chansigi@gmail.com>
2023-07-24 13:40:10 +0900

    .

--- 2_전처리기5.c
+++ 2_전처리기5.c
@@ -35,26 +35,34 @@ int main(void)
 #include <stdio.h>
 
 int operations = 0; /* 전체 함수 호출 횟수 */
-int calls_to_f = 0; /* F() 함수 호출 횟수 */
-int calls_to_g = 0; /* G() 함수 호출 횟수 */
+int calls_to_f = 0; /* f() 함수 호출 횟수 */
+int calls_to_g = 0; /* g() 함수 호출 횟수 */
 
-// 매크로 함수가 결과를 제공할 때
-#define F(x) (++operations, ++calls_to_f, 2 * (x))
-#define G(x) (++operations, ++calls_to_g, (x) + 1)
+static inline int f(int x)
+{
+    ++operations;
+    ++calls_to_f;
 
-// 문제점
-// 1) 동일한 지역변수가 제공될 경우, 문제가 발생합니다.
-// 2) 미정의 동작
+    return 2 * x;
+}
+
+static inline int g(int x)
+{
+    ++operations;
+    ++calls_to_g;
+
+    return x + 1;
+}
+
 int main(void)
 {
     // , - 나열 연산자
-    printf("%d\n", (1, 2, 3)); // 3
+    // printf("%d\n", (1, 2, 3)); // 3
 
     int result;
     int x = 10;
 
-    result = F(x) + G(x);
-    // result = (++operations, ++calls_to_f, 2 * (x)) + (++operations, ++calls_to_g, (x) + 1);
+    result = f(x) + g(x);
 
     printf("result: %d\n", result);
     printf("%d %d %d\n", operations, calls_to_f, calls_to_g);
