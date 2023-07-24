674b36cadc10efad79718d1c0258fd3d2af8bbb2
Chansik Yun <chansigi@gmail.com>
2023-07-25 08:47:31 +0900

    .

--- 2_전처리기11.c
+++ 2_전처리기11.c
@@ -1,12 +1,12 @@
 // 2_전처리기11.c
 #include <stdio.h>
 
-// #define cstring char*
+#define cstring char*
 // 문제점: 매크로 정의는 타입에 대한 범위 규칙이 제대로 적용되지 않습니다.
 // 해결방법
 //  => 타입에 대해서 재정의할 때는 typedef를 이용해야 합니다.
 
-typedef char* cstring;
+// typedef char* cstring;
 
 int main(void)
 {
@@ -20,6 +20,7 @@ int main(void)
     printf("%zd\n", sizeof(s1));
     printf("%zd\n", sizeof(s2));
 
+    // 권장되지 않습니다.
     char *s3, *s4;
     // s3 -> char*
     // s4 -> char*
