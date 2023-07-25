271e5423d58a49488e3b4a01274c6810cdff404f
Chansik Yun <chansigi@gmail.com>
2023-07-25 09:34:46 +0900

    .

--- complex.h
+++ complex.h
@@ -1,7 +1,11 @@
 
+// #pragma once
+// 대부분의 컴파일러가 지원하지만,
+// 표준이 아닙니다.
+
 // project_path_name_H
-#ifndef COMPLEX_H
-#define COMPLEX_H
+#ifndef PROJECT_UTILS_COMPLEX_H
+#define PROJECT_UTILS_COMPLEX_H
 
 struct complex {
     int real;
