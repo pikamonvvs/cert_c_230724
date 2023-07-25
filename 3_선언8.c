c2f9dd290f43643c938285c9c8b83d39af297e7a
Chansik Yun <chansigi@gmail.com>
2023-07-25 14:38:54 +0900

    .

---
+++ 3_선언8.c
@@ -0,0 +1,26 @@
+// 3_선언8.c
+#include <stdio.h>
+
+// 1. 동일한 가시성을 가지는 식별자는
+//    타입포그라피적으로 이름이 모호하지 않아야 한다.
+
+int main(void)
+{
+    int id_I;
+    int id_l;
+    int id_1;
+
+    int id_Z;
+    int id_2;
+
+    int id_0;
+    int id_O;
+
+    int id_8;
+    int id_B;
+
+    int id_m;
+    int id_rn;
+
+    return 0;
+}
