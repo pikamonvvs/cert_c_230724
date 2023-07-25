5cc25d732fa26e5f251194dc37cae839e702f477
Chansik Yun <chansigi@gmail.com>
2023-07-25 15:45:57 +0900

    .

--- 3_선언9.c
+++ 3_선언9.c
@@ -54,7 +54,7 @@ int main(void)
 //  해결 방법
 //   - 에러를 반환하는 함수는 함수의 반환 타입을 에러 타입으로 사용하는 것이 좋습니다.
 //   - 에러 코드도 매직 넘버가 아닌 심볼릭 상수를 사용하는 것이 좋습니다.
-
+#if 1
 typedef int error_t;
 enum {
     DIVISION_BY_ZERO = -100,
@@ -91,3 +91,53 @@ int main(void)
 
     return 0;
 }
+#endif
+
+#if 0
+// 1. errno 전역 변수를 통해 에러의 원인을 파악할 수 있습니다.
+#include <errno.h>
+
+// 2. errno를 문자열로 변경하는 기능을 제공합니다. - strerror
+#include <string.h>
+
+// 3. perror() 함수를 통해 위의 과정을 수행할 수 있습니다.
+// #include <stdio.h>
+
+int main(void)
+{
+    FILE* fp = fopen("a.txt", "r");
+    if (fp == NULL) {
+        // fprintf(stderr, "fopen() error: %d\n", errno);
+        // fprintf(stderr, "fopen() error: %s\n", strerror(errno));
+        perror("fopen() error");
+        return 1;
+    }
+
+    return 0;
+}
+#endif
+
+// scanf_s, strcpy_s ....
+// => C11 Annex K 확장 라이브러리
+// => 표준에 도입이 되었지만, 강제되지 않습니다.
+// => 오직 윈도우 컴파일러만 지원하고 있습니다.
+
+#if 0
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char buf[10];
+
+    // 'strcpy': This function or variable may be unsafe. Consider using strcpy_s instead
+    // strcpy(buf, "helloworld"); /* 미정의 동작 */
+
+    errno_t error;
+    error = strcpy_s(buf, sizeof buf, "heellworld");
+    printf("error: %d\n", error);
+    // printf("%s\n", buf);
+
+    return 0;
+}
+#endif
