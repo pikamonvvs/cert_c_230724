3f2a3877ac5984a2b871dcbd8edb428cfd8b40e6
Chansik Yun <chansigi@gmail.com>
2023-07-26 13:49:23 +0900

    .

--- 3_선언17.c
+++ 3_선언17.c
@@ -15,8 +15,15 @@
 // => MISRA 에서는 사용을 금지하고 있습니다.
 //  : 지원되지 않는 프로세서를 사용하는 경우, 성능향상도 존재하지 않고
 //    미세한 성능 향상을 위해 미정의 동작으로 인한 위험성을 감수할 필요가 없습니다.
-void print_array(int data[static 3], int n)
+
+enum {
+    ARRAY_SIZE = 3
+};
+
+void print_array(int data[static ARRAY_SIZE], int n)
 {
+    printf("size: %zu\n", sizeof(data)); // 8
+
     for (int i = 0; i < n; i++) {
         printf("%d\n", data[i]);
     }
