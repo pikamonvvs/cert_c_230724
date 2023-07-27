6e30eeb24d57538804e9b710f68d6e1432b8ff74
Chansik Yun <chansigi@gmail.com>
2023-07-27 14:55:25 +0900

    .

--- 4_표현식13.c
+++ 4_표현식13.c
@@ -58,6 +58,7 @@ int main(void)
 }
 #endif
 
+#if 0
 struct AAA {
     void (*f)(struct AAA* p);
 };
@@ -97,6 +98,45 @@ int main(void)
     aaa.f = foo;
     struct AAA* r = g(&aaa);
     aaa.f(r);
+
+    return 0;
+}
+#endif
+
+int global = 0;
+int f1(void)
+{
+    global = 10;
+    return 10;
+}
+
+int f2(void)
+{
+    global = 20;
+    return 20;
+}
+
+int main(void)
+{
+    int result;
+
+    // * 함수 호출을 이항 연산자에서 사용할 경우,
+    //   어떤 함수가 먼저 호출될지는 미지정 동작입니다.
+    // - 해결 방법: 분리해서 사용해야 합니다.
+    result = f1() + f2(); /* 미지정 동작 */
+    result = f1() - f2(); /* 미지정 동작 */
+    result = f1() * f2(); /* 미지정 동작 */
+    result = f1() / f2(); /* 미지정 동작 */
+    result = f1() % f2(); /* 미지정 동작 */
+
+    //-----
+    result = f1() && f2(); /* OK! - f1()부터 수행됩니다. */
+    result = f1() || f2(); /* OK! - f1()부터 수행됩니다. */
+    result = f1(), f2(); /* OK! - 나열 연산자는 사용하지 않는 것이 좋습니다. */
+    result = f1() ? f2() : 0; /* OK, 삼항 연산자도 f1()부터 수행됩니다. */
+
+    // 핵심: 위의 연산은 미지정 동작이 아니지만, 함수의 호출을 함께 사용하는 것은
+    //      지양해야 합니다.
 
     return 0;
 }
