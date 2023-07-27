faab3fcd11b702c6b8068aecd8146d7415e21d14
Chansik Yun <chansigi@gmail.com>
2023-07-27 13:12:24 +0900

    .

--- 4_표현식9.c
+++ 4_표현식9.c
@@ -11,6 +11,7 @@ struct big {
     int si_5;
 };
 
+// https://cnst.livejournal.com/24040.html
 int f(void)
 {
     size_t skip = offsetof(struct big, ull_2);
@@ -19,7 +20,9 @@ int f(void)
         return -1;
     }
 
-    memset(s + skip, 0, sizeof(struct big) - skip);
+    // 포인터의 산술 연산을 수행할 때, 바이트 단위의 처리를 위해서는
+    // 명시적으로 char* 타입으로의 캐스팅이 필요합니다.
+    memset(s + skip, 0, sizeof(struct big) - skip); /* 잘못된 연산 */
     /* ... */
     free(s);
     s = NULL;
