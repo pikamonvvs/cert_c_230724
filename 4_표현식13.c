ea130afae4b0eaa041ab2681bbf291d2b00278c9
Chansik Yun <chansigi@gmail.com>
2023-07-27 14:46:01 +0900

    .

--- 4_표현식13.c
+++ 4_표현식13.c
@@ -23,6 +23,7 @@ int main(void)
 }
 #endif
 
+#if 0
 void func(int a, int b)
 {
     printf("%d %d\n", a, b);
@@ -52,6 +53,50 @@ int main(void)
     int r1 = f1();
 
     func(r1, r2);
+
+    return 0;
+}
+#endif
+
+struct AAA {
+    void (*f)(struct AAA* p);
+};
+
+void foo(struct AAA* p)
+{
+    printf("foo\n");
+}
+
+void goo(struct AAA* p)
+{
+    printf("goo\n");
+}
+
+struct AAA* g(struct AAA* p)
+{
+    p->f = goo;
+    return p;
+}
+
+int main(void)
+{
+    struct AAA aaa = { .f = foo };
+    // aaa.f(NULL);
+
+    // g(&aaa)의 결과로 f가 goo로 호출이 될 것인지,
+    // 바뀌기 이전의 f인 foo가 호출될 것인지
+    // => 미지정 동작입니다.
+
+    // * 함수 포인터로 함수를 호출할 때, 인자를 통해 함수 포인터의 값을
+    //   변경하는 행위는 안됩니다.
+    // aaa.f = foo;
+    // aaa.f(g(&aaa));
+
+    // * 해결 방법: 함수의 호출 인자 안에서 부수 효과를 가지는 형태로
+    //            코드를 작성하면 안됩니다.
+    aaa.f = foo;
+    struct AAA* r = g(&aaa);
+    aaa.f(r);
 
     return 0;
 }
