2e2b26f31233205cb6791ccfd0d056dfcf9e9224
Chansik Yun <chansigi@gmail.com>
2023-07-27 10:51:54 +0900

    .

--- 4_표현식7.c
+++ 4_표현식7.c
@@ -1,5 +1,6 @@
 // 4_표현식7.c
 #include <stdio.h>
+#include <stddef.h> // offsetof
 
 struct list_head {
     struct list_head* next;
@@ -9,7 +10,7 @@ struct user {
     char name[32];
     int age;
     // ...
-
+    double height;
     struct list_head link;
 };
 
@@ -19,6 +20,7 @@ void insert(struct list_head* head, struct list_head* new)
     head->next = new;
 }
 
+#if 0
 int main(void)
 {
     struct user user1 = {
@@ -38,9 +40,37 @@ int main(void)
 
     struct list_head* current = head.next;
     while (current) {
+        // struct user* 를 통해서 데이터를 출력하기 위해서는
+        // 현재 link의 offset을 기준으로 계산이 필요합니다.
+
         printf("XXX\n");
         current = current->next;
     }
+
+    return 0;
+}
+#endif
+
+// 구조체 멤버의 오프셋을 구하는 방법
+//  - C에서 &* 가 만나면 상쇄됩니다.
+//  - (*p).link
+//     p->link
+#define xoffsetof(s, m) ((unsigned long)&((s*)0)->m)
+
+int main(void)
+{
+    struct user user1 = {
+        .name = "Tom", .age = 42
+    };
+
+    // (unsigned long)&(*(struct user*)0).link => link 위치를 참조하는 연산으로 변환됩니다.
+    // printf("%lu\n", (unsigned long)&(*(struct user*)0).link);
+    // printf("%lu\n", (unsigned long)&((struct user*)0)->link);
+    //  => C 표준에서 패딩을 고려한 구조체 멤버의 offset을 구하는 기능을 제공합니다.
+    //   : offsetof
+
+    printf("%lu\n", xoffsetof(struct user, link));
+    printf("%lu\n", offsetof(struct user, link));
 
     return 0;
 }
