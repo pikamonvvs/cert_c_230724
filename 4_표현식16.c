95978d0b944eb8632b7ad4e0b0c52306bc76c88f
Chansik Yun <chansigi@gmail.com>
2023-07-27 16:20:25 +0900

    .

--- 4_표현식16.c
+++ 4_표현식16.c
@@ -7,7 +7,7 @@
 // 1) goto 문은 반드시 goto 이후에 선언된 같은 함수의 라벨에 대해서만
 //    점프를 수행해야 합니다.
 //     => 이후에 수행되는 라벨에 대해서만 허용합니다.
-
+#if 0
 int main(void)
 {
     int a = 10;
@@ -23,5 +23,55 @@ restart:
     }
 
 fail:
+    return 0;
+}
+#endif
+
+// goto가 유용할 때
+// 1) 중첩된 반복문을 탈출할 때, flag를 사용하는 것보다
+//    goto 사용하는 것이 유용할 수 있습니다.
+
+#if 0
+int main(void)
+{
+    int n = 5;
+    int flag = 0;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; ++j) {
+
+            if (i + j == n) {
+                // 전체 루프를 탈출하고 싶습니다.
+                printf("---- break ----\n");
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag) {
+            break;
+        }
+    }
+
+    return 0;
+}
+#endif
+
+int main(void)
+{
+    int n = 5;
+    int flag = 0;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; ++j) {
+
+            if (i + j == n) {
+                printf("---- goto ----\n");
+                goto out;
+            }
+        }
+    }
+out:
+
     return 0;
 }
