f391833a82d8ac15af9d8b8a55a9059cfe9a1953
Chansik Yun <chansigi@gmail.com>
2023-07-28 10:48:35 +0900

    .

--- 5_타입3.c
+++ 5_타입3.c
@@ -11,8 +11,15 @@
 //  "이항 연산의 경우 두개의 피 연산자가 동일한 타입으로 변환됩니다."
 
 // 1) 같은 타입이면 변환하지 않습니다.
+//  예외) 프로모션: int보다 작은 타입의 경우,
+//              프로모션 후 수행됩니다.
+#if 0
 int main(void)
 {
+    int8_t c1 = 10;
+    int8_t c2 = 20;
+    printf("%d\n", c1 + c2); // promotion => int
+
     int a = 100;
     int b = 200;
     printf("%d\n", a + b); // int
@@ -20,6 +27,69 @@ int main(void)
     long la = 100L;
     long lb = 200L;
     printf("%ld\n", la + lb); // long
+
+    return 0;
+}
+#endif
+
+// 2) 두 개의 피연산자가 정수 타입이면, 범위가 큰 타입으로 변환됩니다.
+#if 0
+int main(void)
+{
+    int a = 10;
+    long la = 100L;
+
+    printf("%ld\n", a + la); // long
+
+    long lb = 10000L;
+    long long llb = 2000000LL;
+    printf("%ld\n", lb + llb);
+
+    return 0;
+}
+#endif
+
+// 3) 부호 없는 정수 타입의 피연산자가 다른 피연산자의 순위보다 크거나 같은 경우
+//    부호 있는 정수 타입의 피연산자는 부호 없는 정수 타입으로 변환됩니다.
+#if 0
+int main(void)
+{
+    unsigned int ua = 10U;
+    int a = 100;
+    printf("%u\n", ua + a); // unsigned int
+
+    unsigned long lb = 100L;
+    int b = 10000;
+    printf("%lu\n", lb + b); // unsigned long
+
+    return 0;
+}
+#endif
+
+// 4) 부호 있는 정수 타입의 피연산자가 부호 없는 타입의 모든 값을
+//    표현할 수 있는 경우, 부호 없는 정수 타입의 피연산자는
+//    부호 있는 정수 타입으로 변환됩니다.
+#if 0
+int main(void)
+{
+    long long a = 10LL;
+    unsigned int ua = 11000U;
+    printf("%lld\n", a + ua); // long long
+
+    return 0;
+}
+#endif
+
+// 5) 부호 있는 정수 타입의 피연산자가 부호 없는 타입의 모든 값을
+//    표현할 수 없는 경우, 부호 있는 정수 타입의 피연산자가 동일 타입의
+//    부호 없는 타입으로 두 피연산자 모두 변환됩니다.
+int main(void)
+{
+    // 둘다 8바이트 입니다.
+    long long lla = 1000LL;
+    unsigned long la = 10000L;
+
+    printf("%llu\n", lla + la);
 
     return 0;
 }
--- 5_타입3.cpp
+++ 5_타입3.cpp
@@ -28,11 +28,20 @@ constexpr auto type_name()
 
 #define PRINT_TYPE(x) cout << type_name<decltype(x)>() << endl
 
+// $ g++ 5_타입3.cpp -std=c++17
+// 1) 같은 타입이면 변환하지 않습니다.
+//  예외) 프로모션: int보다 작은 타입의 경우,
+//              프로모션 후 수행됩니다.
+#if 0
 int main()
 {
     int8_t c1 = 10;
     int8_t c2 = 20;
     PRINT_TYPE(c1 + c2); // promotion => int
+
+    int16_t s1 = 10;
+    int16_t s2 = 20;
+    PRINT_TYPE(s1 + s2); // promotion => int
 
     int a = 100;
     int b = 200;
@@ -42,4 +51,57 @@ int main()
     long lb = 200L;
 
     PRINT_TYPE(la + lb); // long
+}
+#endif
+
+// 2) 두 개의 피연산자가 정수 타입이면, 범위가 큰 타입(상위 랭크 타입)으로 변환됩니다.
+#if 0
+int main()
+{
+    int a = 10;
+    long la = 100L;
+
+    PRINT_TYPE(a + la); // long
+
+    long lb = 10000L;
+    long long llb = 2000000LL;
+    PRINT_TYPE(lb + llb);
+}
+#endif
+
+// 3) 부호 없는 정수 타입의 피연산자가 다른 피연산자의 순위보다 크거나 같은 경우 부호 있는 정수 타입의 피연산자는 부호 없는 정수 타입으로 변환됩니다.
+#if 0
+int main()
+{
+    unsigned int ua = 10U;
+    int a = -100;
+    PRINT_TYPE(ua + a); // unsigned int
+
+    unsigned long lb = 100L;
+    int b = 10000;
+    PRINT_TYPE(lb + b); // unsigned long
+}
+#endif
+
+// 4) 부호 있는 정수 타입의 피연산자가 부호 없는 타입의 모든 값을
+//    표현할 수 있는 경우, 부호 없는 정수 타입의 피연산자는
+//    부호 있는 정수 타입으로 변환됩니다.
+#if 0
+int main()
+{
+    long long a = 10LL;
+    unsigned int ua = 11000U;
+    PRINT_TYPE(a + ua); // long long
+}
+#endif
+
+// 5) 부호 있는 정수 타입의 피연산자가 부호 없는 타입의 모든 값을
+//    표현할 수 없는 경우, 부호 있는 정수 타입의 피연산자가 동일 타입의
+//    부호 없는 타입으로 두 피연산자 모두 변환됩니다.
+int main()
+{
+    long long lla = 1000LL;
+    unsigned long la = 10000L;
+
+    PRINT_TYPE(lla + la); // unsigned long long
 }
